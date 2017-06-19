76610daba1b0cbca03ef167dc4ce2747325f912f
James Mountain <james.w.mountain@gmail.com>
2017-06-19 15:41:37 +0000

    scala base files

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by James Mountain on 19/06/2017.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+}
---
+++ src/test/scala/GameStoreTest.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by James Mountain on 19/06/2017.
+  */
+object GameStoreTest {
+
+}

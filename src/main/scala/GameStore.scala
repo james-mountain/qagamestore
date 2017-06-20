0a870340d97a6cd8f673ddd32cb0d84d708312d8
James Mountain <james.w.mountain@gmail.com>
2017-06-20 10:23:25 +0000

    Started GameStore
    Added tests which fail for gamestore
    Added parameter in gamestore for the items in store

---
+++ src/main/scala/GameStore.scala
@@ -0,0 +1,15 @@
+/**
+  * Created by Administrator on 20/06/2017.
+  */
+class GameStore {
+  var items : List[Item] = List.empty[Item]
+  //var employees = List[Employee]
+  //var customers = List[Customers]
+  //var receipts == List[Receipt]
+
+  def getItems() = items
+
+  def addItem() = {}
+  def updateItem(itemid : Int) = {}
+  def deleteItem() = {}
+}
--- src/test/scala/GameStoreTest.scala
+++ src/test/scala/GameStoreTest.scala
@@ -1,6 +1,28 @@
 /**
   * Created by James Mountain on 19/06/2017.
   */
-object GameStoreTest {
+import org.scalatest._
 
+class GameStoreSpec extends FlatSpec with Matchers {
+  val gamestore = new GameStore
+
+  "Game Store" should "be able to add items for sale" in {
+    gamestore.addItem()
+    gamestore.getItems().length shouldBe 1
+  }
+
+  "Game Store" should "be able to remove items from sale" in {
+    gamestore.getItems().length shouldBe 1
+    gamestore.deleteItem()
+    gamestore.getItems().length shouldBe 0
+  }
+
+  "Game Store" should "be able to update item information" in {
+    val itemid = 5
+
+    gamestore.addItem()
+    gamestore.getItems().length shouldBe 1
+    gamestore.updateItem(5)
+    gamestore.getItems().filter(item => item.getID() == itemid).head.getStockPrice() == 50.00
+  }
 }

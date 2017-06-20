ad9afe2eb55b2f509da5fa9928bfa2a69d68c432
Lewis Hay <calinu_4u@yahoo.com>
2017-06-20 11:47:24 +0000

    Receipt almost complete and first gui attempt

--- src/test/scala/ReceiptTestSpec.scala
+++ src/test/scala/ReceiptTestSpec.scala
@@ -6,22 +6,24 @@ import org.scalatest._
 import java.time.LocalDate
 import scala.collection.mutable.ListBuffer
 class ReceiptTestSpec extends FlatSpec{
-  var r=new Receipt(1,ListBuffer[Double](),0,ListBuffer[String](),"Cash")
+  var r=new Receipt(1)
+  r.setPaymentType("Cash")
 
   "A receipt " should "have all the right information displayed " in{
     assert(r.getId()==1 &&r.getTotal()==0&&r.getPaymentType()=="Cash")
   }
- // r.addPrice(15)
-  r.addItem(new Item(1,"First",10,15,150,"PC"),2)
+
+  var r1=new Receipt(2)
+  r1.addItem(new Item(1,"First",10,15,150,"PC"),2)
   "Add a price into the receipt and check size of list " should "be 1 " in{
-    assert(r.getPrices().size==1)
+    assert(r1.getPrices().size==1)
   }
 
 
   "Add an item to the receipt list" should "have list size equal to 1" in{
-    assert(r.getItems().size==1)
+    assert(r1.getItems().size==1)
   }
   "The to string method" should "display current date" in{
-    assert(r.date==LocalDate.now().toString)
+    assert(r1.date==LocalDate.now().toString)
   }
 }

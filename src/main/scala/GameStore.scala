5785dcb5b2eed931b34dd32cd0e0c9171b8fc768
James Mountain <james.w.mountain@gmail.com>
2017-06-20 10:53:17 +0000

    Made 2 tests work on GameStore
    Removed spaces from games file
    Loaditems basic functionality and additems to items to sale functionality

--- src/main/scala/GameStore.scala
+++ src/main/scala/GameStore.scala
@@ -1,17 +1,28 @@
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
+
 /**
   * Created by Administrator on 20/06/2017.
   */
 class GameStore {
-  var items : List[Item] = List.empty[Item]
+  var items : ListBuffer[Item] = ListBuffer.empty[Item]
   //var employees = List[Employee]
   //var customers = List[Customers]
   //var receipts == List[Receipt]
 
-  def loadItems() = {}
+  def loadItems() = {
+    val fileItems = Source.fromFile("C:/Users/Administrator/Downloads/patmat/QAGameStore/src/main/scala/games").getLines().toList
+    val fileStrings = fileItems.map(str => str.split(","))
+
+    for (line <- fileStrings) {
+      val newItem = new Item(line(0).toInt, line(1), line(2).toDouble, line(3).toDouble, line(4).toInt, line(5))
+      addItem(newItem)
+    }
+  }
 
   def getItems() = items
 
-  def addItem() = {}
+  def addItem(item : Item) = items += item
   def updateItem(itemid : Int) = {}
   def deleteItem() = {}
 }
--- src/test/scala/GameStoreTest.scala
+++ src/test/scala/GameStoreTest.scala
@@ -5,9 +5,10 @@ import org.scalatest._
 
 class GameStoreSpec extends FlatSpec with Matchers {
   val gamestore = new GameStore
+  val newitem = new Item(1, "XBOX", 500.00, 500.0, 10, "Console")
 
   "Game Store" should "be able to add items for sale" in {
-    gamestore.addItem()
+    gamestore.addItem(newitem)
     gamestore.getItems().length shouldBe 1
   }
 
@@ -20,7 +21,7 @@ class GameStoreSpec extends FlatSpec with Matchers {
   it should "be able to update item information" in {
     val itemid = 5
 
-    gamestore.addItem()
+    gamestore.addItem(newitem)
     gamestore.getItems().length shouldBe 1
     gamestore.updateItem(5)
     gamestore.getItems().filter(item => item.getID() == itemid).head.getStockPrice() == 50.00

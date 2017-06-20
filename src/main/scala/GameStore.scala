00223fc8fcee9f8dafd3b2ff175a94449e848fae
Administrator <alex.rimmer@qa.com>
2017-06-20 13:53:53 +0100

    Add employee and add customer functions

--- src/main/scala/GameStore.scala
+++ src/main/scala/GameStore.scala
@@ -8,15 +8,22 @@ import scala.io.Source
   */
 class GameStore {
   var items : ListBuffer[Item] = ListBuffer.empty[Item]
-  //var employees = List[Employee]
-  //var customers = List[Customers]
-  //var receipts == List[Receipt]
+  var employees : ListBuffer[Employee] = ListBuffer.empty[Employee]
+  var customers : ListBuffer[Customer] = ListBuffer.empty[Customer]
+  var receipts = List[Receipt]
 
   def getItems() = items
+  def getEmployees() : ListBuffer[Employee] = { employees }
+  def getCustomers() : ListBuffer[Customer] = { customers }
+
   def addItem(item : Item) = items += item
+  def addEmployee(employee : Employee) : Unit = { employees += employee }
+  def addCustomer(customer : Customer) : Unit = { customers += customer }
+
   def updateItem(itemid : Int) : Item = {
     items.filter(item => item.getID() == itemid).head
   }
+
   def deleteItem(itemid : Int) : Boolean = {
     items.find(item => item.getID() == itemid) match {
       case None => false

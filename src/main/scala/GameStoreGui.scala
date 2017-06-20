dfac4dc891e2ecae05d211f2407a85a42287f2da
armani2k20 <armani2k20@gmail.com>
2017-06-20 17:24:36 +0000

    fixed gui orientation

--- src/main/scala/GameStoreGui.scala
+++ src/main/scala/GameStoreGui.scala
@@ -2,66 +2,58 @@ import scala.swing._
 import scala.swing.event._
 
 class UI extends MainFrame {
-  def restrictHeight(s: Component) {
-    s.maximumSize = new Dimension(Short.MaxValue, s.preferredSize.height)
-  }
 
   title = "Game Store"
 
   val emailField = new TextField {
-    columns = 32
+    columns = 20
   }
-
   val passwordField = new TextField {
-    columns = 32
+    columns = 20
   }
-
-  restrictHeight(emailField)
 
   contents = new BoxPanel(Orientation.Vertical) {
     contents += new BoxPanel(Orientation.Horizontal) {
       contents += new Label("Email Address: ")
-      contents += new Label("Password: ")
-      contents += Swing.HStrut(5)
       contents += emailField
+    }
+
+    contents += new BoxPanel(Orientation.Horizontal) {
+      contents += new Label("Password:         ")
       contents += passwordField
     }
-    contents += Swing.VStrut(5)
-    contents += new BoxPanel(Orientation.Horizontal) {
-      contents += Swing.HStrut(20)
-    }
-    contents += Swing.VStrut(5)
-    contents += Swing.VStrut(3)
-    contents += Swing.VStrut(5)
     contents += new BoxPanel(Orientation.Horizontal) {
       contents += Swing.HGlue
+      contents += Button("Login") {
+        //login functionality
+      }
+
       contents += Button("Close") {
         reportAndClose()
       }
     }
+
     for (e <- contents)
       e.xLayoutAlignment = 0.0
     border = Swing.EmptyBorder(10, 10, 10, 10)
-  }
+  } // end of main contents
 
   listenTo(emailField)
   listenTo(passwordField)
 
+
   reactions += {
-    case EditDone(`emailField`) =>
-      println("Your email is now: " + emailField.text)
-    case EditDone(`passwordField`) =>
-      println("Your password is now: " + passwordField.text)
     case ButtonClicked(s) =>
       println("Button click on button: '" + s.text + "'")
   }
 
+  // close button functionality
   def reportAndClose() {
     println("Your email: " + emailField.text)
     println("Your password: " + passwordField.text)
     sys.exit(0)
   }
-}
+} // end of class
 
 object GameStoreGui {
   def main(args: Array[String]) {
